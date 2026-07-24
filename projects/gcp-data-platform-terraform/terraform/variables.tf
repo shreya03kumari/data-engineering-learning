7c2db48f7ed3174df631a5e1e29fc05cd8a9dfc6
Shreya Kumari <48504889+shreya03kumari@users.noreply.github.com>
2026-07-24 17:55:57 +0200

    Create variables.tf

---
+++ projects/gcp-data-platform-terraform/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+
+  description = "GCP Project ID"
+
+  type = string
+
+}
+
+
+
+variable "region" {
+
+  description = "GCP Region"
+
+  type = string
+
+  default = "europe-west1"
+
+}
